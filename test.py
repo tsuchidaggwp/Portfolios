240007b7d2243af26e26f5e4e3184548f4c81bed
tsuchida <tsuchidakoyo@gmail.com>
2024-04-15 16:17:46 +0900

    commit

--- test.py
+++ test.py
@@ -2,9 +2,12 @@ number = input("Please enter an integer: ")
 
 number = int(number)
 sum = 0
+double_sum = 0
 
-for i in range(number):
+for i in range(number+1):
     sum += i
+    double_sum += i*i
 
    
 print(f'1から{number}までの和は{sum}です。')
+print(f'1から{number}までの二乗和は{double_sum}です。')
